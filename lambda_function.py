94fcb4f6ce03ffe4935c6d12781ff5165abaf56a
Daniel Furman <djfurman@gmail.com>
2019-10-02 19:01:51 -0400

    Add http request translation.

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,34 @@
-from alb_response import alb_response
+import base64
+import json
 from http import HTTPStatus
+
+from alb_response import alb_response
+
+import dynamo
+import person_controller
 
 
 def lambda_handler(event, context):
-    return alb_response(
-        http_status=HTTPStatus.CREATED,
-        json={"token": "5a0dec97-1b4c-44bc-964f-23f7617b6081"},
-    )
+    request = fetch_http_request(event)
+
+    person_to_tokenize = person_controller.build_person_store(request)
+
+    token = dynamo.add_record(person_to_tokenize)
+    return alb_response(http_status=HTTPStatus.CREATED, json={"token": token})
+
+
+def fetch_http_request(event):
+    if event["isBase64Encoded"]:
+        payload = base64.b64decode(event["body"])
+    else:
+        payload = event["body"]
+
+    if isinstance(payload, bytes):
+        body = payload.decode("utf-8")
+
+    return {
+        "headers": event["headers"],
+        "body": json.loads(body),
+        "method": event["httpMethod"],
+        "path": event["path"],
+    }
